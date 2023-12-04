f5ecc46dd0adaf811a35c07310fd9a8b78bf14fb
alu10722437 <eruk11000@gmail.com>
2023-12-04 09:09:06 +0100

    Inicio de proyecto

---
+++ src/Coches.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Coches {
+    public static void main(String[] args) {
+
+        Scanner inputValue = new Scanner(System.in);
+
+        ArrayList<String> coches = new ArrayList<>();
+
+        String coche;
+
+        do{
+            coche = inputValue.nextLine();
+            if (coche.isEmpty()){
+                coches.add(coche);
+            }
+        }while (coche.isEmpty());
+
+        Collections.sort(coches);
+
+        System.out.println(coches);
+
+    }
+}
